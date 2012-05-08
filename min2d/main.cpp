bb17afc3aaa533940b0ec87045c41b2792b3888c
= <tobstu@gmail.com>
2012-05-08 19:30:32 +0200

    feierabend

--- min2d/main.cpp
+++ min2d/main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <vector>
-#include <math.h>
 #include <climits>
+#include <stdint.h>
 
 using namespace std;
 
 int findMin(int xmin, int ymin, int xmax, int ymax, vector<vector<vector<int > > > &fields, int pot);
+static inline uint32_t log2(const uint32_t x); 
 
 int main() {
 	int cols, lines;
@@ -27,10 +28,10 @@ int main() {
 	vector < vector <int > > source = field;
 	int original_lines = lines;
 	int original_cols = cols;
-	lines = lines & ~3;
-	cols = cols & ~3;
-	while(pot <= original_cols && pot <= original_lines) {
-//		cout << "lines: " << lines << ", cols: " << cols << ", pot: " << pot << endl;
+	lines = (lines >> 1) << 1;
+	cols = (cols >> 1) << 1;
+	while(1 << pot <= original_cols && 1 << pot <= original_lines) {
+		cout << "lines: " << lines << ", cols: " << cols << ", pot: " << pot << endl;
 		vector< vector< int > > result;
 		int y = 0;
 		int x = 0;
@@ -52,16 +53,23 @@ int main() {
 			}
 
 		fields.push_back(result);
+		//////////////////////////////
+		cout << "-----------------" << endl << "table for " << pot << endl;
+		for(int i = 0; i < lines >> 1; i++) {
+			for(int j = 0; j < cols >> 1; j++)
+				cout << result[i][j];
+			cout << endl;
+		}
+		//////////////////////////////
 		source = result;
-		cout << pot << endl;
-		pot = pot << 1;
-		lines = (lines >> 1) & ~3;
-		cols = (cols >> 1) & ~3;
+		pot++;
+		lines = (lines >> 1);
+		cols = (cols >> 1);
 	}
 	cout << "read for requests" << endl;
 	int xmin, ymin, xmax, ymax;
 		while(cin >> xmin >> ymin >> xmax >> ymax) {
-		int result = findMin(xmin - 1, ymin - 1, xmax - 1, ymax - 1, fields, pot);
+		int result = findMin(xmin, ymin, xmax, ymax, fields, pot);
 		cout << "result: " << result << endl;
 	}
 }
@@ -70,29 +78,40 @@ int findMin(int xmin, int ymin, int xmax, int ymax, vector<vector<vector<int > >
 
 	cout << "request: (" << xmin << "," << ymin << ")x(" << xmax << "," << ymax << ") with maxpot " << maxpot << ".";
 	//abort if there is no area
-/*	if(xmin - xmax == 0 || ymin - ymax == 0) {
+	if(xmin - xmax == 0 || ymin - ymax == 0) {
 		cout << "abort - too small" << endl;
 		return INT_MAX;
 	}
-*/
-	//calc max mathing square-size
-	int size = 1;
-	int pot = 0;
-	while(!((xmax - xmin) & size) && !((ymax - ymin) & size) && pot <= maxpot) {
-		size = size << 1;
-		pot++;
+
+/*	//calc max mathing square-size
+	int size = 1 << maxpot;
+	int pot = maxpot;
+	while(pot > (log2(xmax) - log2(xmin)) && pot > (log2(ymax) - log2(ymin))){
+		size = size >> 1;
+		pot--;
 	}
-	if(size == 0)
-		return INT_MAX;
 
 	cout << "mathing pot is " << pot << " (" << size << ")" << ".";
 	//calc the left upper edge of matching area
-	int xrelmin, yrelmin, xrelmax, yrelmax;
-	xrelmin = xmin >> pot;
-	yrelmin = ymin >> pot;
-	xrelmax = xmax >> pot;
-	yrelmax = ymax >> pot;
-	/*
+	int xrelmin, yrelmin = 0;
+	int xrelmax = xmax;
+	int yrelmax = ymax;
+	if(pot > 0) {
+		xrelmin = ((xmin + (xmin % size)) >> (pot) / size);
+		yrelmin = ((ymin + (ymin % size)) >> (pot) / size);
+		xrelmax = ((xmax - (xmax % size)) >> (pot) / size);
+		yrelmax = ((ymax - (ymax % size)) >> (pot) / size);
+		//xrelmin = (xmin / size) + (xmin % size);
+		//yrelmin = (ymin / size) + (ymin % size);
+		//xrelmax = (xmax / size);// - (xmax % size);
+		//yrelmax = (ymax / size);// - (ymax % size);
+	} else {
+		xrelmin = xmin;
+		yrelmin = ymin;
+		xrelmax = xmax;
+		yrelmax = ymax;
+	}
+	// *
 	while(xrelmin * size < xmin)
 		xrelmin++;
 	while(yrelmin * size < ymin)
@@ -108,33 +127,52 @@ int findMin(int xmin, int ymin, int xmax, int ymax, vector<vector<vector<int > >
 		yrelmax++;*/
 
 
-	cout << "matching fields from (" << xrelmin * size << "," << yrelmin * size << ")x(" << xrelmax * size << "," << yrelmax * size << ")" << ".";
+	unsigned int xrelmin = log2(xmin) + 1;
+	unsigned int xrelmax = log2(xmax);
+	unsigned int yrelmin = log2(ymin) + 1;
+	unsigned int yrelmax = log2(ymax);
+	if(1 << xrelmax < xmax)
+		xrelmax++;
+	if(1 << yrelmax < ymax)
+		yrelmax++;
+	int pot = 0;
+	if(xrelmin < yrelmin)
+		pot = xrelmin;
+	else
+		pot = yrelmin;
+	cout << "matching fields from (" << xrelmin << "," << yrelmin << ")x(" << xrelmax << "," << yrelmax << ")" << "." << endl;
 	//calc the minimum inside the area
 	int currMin = INT_MAX;
-	for(int x = xrelmin; x <= xrelmax; x++)
-		for(int y = yrelmin; y <= yrelmin; y++)
-			if(fields[pot][x][y] < currMin)
+	for(int x = xrelmin -1; x < xrelmax; x++)
+		for(int y = yrelmin - 1; y < yrelmax; y++) {
+			cout << fields[pot][x][y] << "(" << pot << "," << x << "," << y << ") < " << currMin << "?" << endl;
+			if(fields[pot][x][y] < currMin) {
 				currMin = fields[pot][x][y];
-	if(pot > 0) {
+				cout << "wrote " << currMin << endl;
+			}
+		}
+	if(maxpot > 0) {
 	//calc the minimum at the borders
 	int tmp[4];
-	if(yrelmax * size != ymax + 1) {
+	for(unsigned int i = 0; i < 4; i++)
+		tmp[i] = INT_MAX;
+	if(1 >> yrelmax != ymax) {
 		cout << "top";
-		tmp[0] = findMin(xmin, ymin, xmax, yrelmax * size, fields, maxpot - 1); //top
+		tmp[0] = findMin(xmin, ymin, xmax, 1 >> yrelmax, fields, maxpot - 1); //top
 	}
 
-	if(xrelmax * size != xmax + 1) {
+	if(1 >> xrelmax != xmax) {
 		cout << "left";
-		tmp[1] = findMin(xmin, ymin, xrelmax * size, ymax, fields, maxpot - 1); //left
+		tmp[1] = findMin(xmin, ymin, 1 >> xrelmax, ymax, fields, maxpot - 1); //left
 	}
 
-	if(yrelmin * size != yrelmin) {
+	if(1 >> yrelmin != yrelmin) {
 		cout << "bottom";
-		tmp[2] = findMin(xmin, yrelmin * size, xmax, ymax, fields, maxpot - 1); //bottom
+		tmp[2] = findMin(xmin, 1 >> yrelmin, xmax, ymax, fields, maxpot - 1); //bottom
 	}
-	if(xrelmin * size != xrelmin) {
+	if(1 >> xrelmin != xrelmin) {
 		cout << "right";
-		tmp[3] = findMin(xrelmin * size, ymin, xmax, ymax, fields, maxpot - 1); //right
+		tmp[3] = findMin(1 >> xrelmin, ymin, xmax, ymax, fields, maxpot - 1); //right
 	}
 	for(unsigned int i = 0; i < 4; i++)
 		if(tmp[i] < currMin)
@@ -143,3 +181,13 @@ int findMin(int xmin, int ymin, int xmax, int ymax, vector<vector<vector<int > >
 	cout << "min: " << currMin << endl;
 	return currMin;
 }
+
+static inline uint32_t log2(const uint32_t x) {
+  uint32_t y;
+  asm ( "\tbsr %1, %0\n"
+      : "=r"(y)
+      : "r" (x)
+  );
+  return y;
+}
+

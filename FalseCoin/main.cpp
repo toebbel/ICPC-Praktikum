16e6e5781e7c4314d462c713305ed4fff0baef73
toebbel <tobstu@gmail.com>
2012-05-13 09:05:55 +0200

    implemented data-input

--- FalseCoin/main.cpp
+++ FalseCoin/main.cpp
@@ -6,12 +6,29 @@
 using namespace std;
 
 int main() {
-	int N;
-	cin >> N;
-	for(unsigned int n = 0; n < N; n++) {
-		int result = 0;
+	int M; //# datasets
+	int
+	cin >> M;
+	cin.ignore(); //rest of the line
+	cin.ignore(); //the blank line
+	for(unsigned int m = 0; m < M; m++) {
+		int N; //# of coins
+		int K; //# of weightings
+		cin >> N >> K;
+		int coins[N];
 		
+		for(unsigned int k = 0; k < K; k++) { //make k weightings
+			/** read the weighting **/
+			int pi; //#coins left/right
+			int left[pi], right[pi]; //tmp arrays to read the lines
+			for(unsigned int i = 0; i < pi; i++)
+				cin >> left[pi];
+			for(unsigned int i = 0; i < pi; i++)
+				cin >> right[pi];
+			char op = '=';
+			cin >> op;
 
-		cout << "Testcase #" << (n + 1) << ": " + result << endl;
+			/** process **/
+		}
 	}
 }

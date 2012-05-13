b1b0b4b816a397eb66787463764cbf4f3d35249a
toebbel <tobstu@gmail.com>
2012-05-13 08:56:43 +0200

    added FalseCoin-problem

---
+++ FalseCoin/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+	for(unsigned int n = 0; n < N; n++) {
+		int result = 0;
+		
+
+		cout << "Testcase #" << (n + 1) << ": " + result << endl;
+	}
+}

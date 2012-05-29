8a4e5ff676228618cd30271caf32d3a6e548666e
toebbel <tobstu@gmail.com>
2012-05-29 14:00:56 +0200

    started problem goto-code

---
+++ goto/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	for(unsigned int t = 0; t < t; t++) {
+		int result = 0;
+		
+
+		cout << "Testcase #" << (t + 1) << ": " + result << endl;
+	}
+}

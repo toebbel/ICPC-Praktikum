2fd4f14a38bf1c680aa9b807c0bee959a2273099
= <tobstu@gmail.com>
2012-06-19 17:34:50 +0200

    finished raining

--- raining/main.cpp
+++ raining/main.cpp
@@ -1,23 +1,32 @@
 #include <iostream>
 #include <stack>
+#include <string>
 
 using namespace std;
 
 int main() {
-	stack<int> map;
-	char curr;
-	int pool = 0;
-	int count = 0;
-	while(cin >> curr) {
-		if(curr == '\\')
-			map.push(count);
-		if(curr == '/')
-			if(map.size() > 0) {
-				pool += count - map.top();
-	//			cout << "add " << count - map.top() << endl;
-				map.pop();
-			}
-		count++;
+	int T;
+	cin >> T;
+	for(int t = 0; t < T; t++) {
+		stack<int> map;
+		char curr;
+		int pool = 0;
+		int count = 0;
+		string line;
+		cin >> line;
+		while(count < line.length()) {
+			curr = line[count];
+			if(curr == '\\')
+				map.push(count);
+			if(curr == '/')
+				if(map.size() > 0) {
+					pool += count - map.top();
+		//			cout << "add " << count - map.top() << endl;
+					map.pop();
+				}
+			count++;
+		}
+
+		cout << pool << endl;
 	}
-	cout << pool << endl;
 }

704d734b90dd7a9b9935835905c62abe8d5f655b
= <tobstu@gmail.com>
2012-07-03 17:20:46 +0200

    can read input and calc quere sum

--- smith/main.cpp
+++ smith/main.cpp
@@ -10,6 +10,7 @@ vector<int> primes;
 int main() {
 	//calculate some prime numbers
 	int n = (int)sqrt(10000000001);
+	n = 10; //remove me!
 	primes.push_back(1);
 	for(int i = 2; i < n; i++) {
 		int candidate = i;
@@ -24,4 +25,17 @@ int main() {
 	//		cout << candidate << ", ";
 		}
 	}
+	
+	int sum_input, input, tmp;
+	string line;
+	int digit = 1;
+	getline(cin, line);
+	for(int i = line.length() - 1; i >= 0; i--) {
+		tmp = line[i] - 48;
+		cout << tmp << "-";
+		sum_input = line[i];
+		input += digit * tmp;
+		digit = digit * 10;
+	}
+	cout << "=" << input << endl;
 }

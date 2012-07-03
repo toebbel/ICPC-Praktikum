5adb305be04326ed6860728a2d0656f20a9c6c30
= <tobstu@gmail.com>
2012-07-03 18:30:59 +0200

    many fixes, still one error left :-(

--- smith/main.cpp
+++ smith/main.cpp
@@ -6,16 +6,17 @@
 using namespace std;
 
 vector<int> primes;
+bool isSmith(int line);
+bool isPrime(int line);
 
 int main() {
 	//calculate some prime numbers, because it's fun!
-	int n = (int)sqrt(10000000001);
-	n = 10; //remove me!
-	primes.push_back(1);
+	int n = sqrt(1000000001);
+//	n =(int) sqrt(4937775); //remove me!
 	for(int i = 2; i < n; i++) {
 		int candidate = i;
 		bool prime = true;
-		for(int j = 2; j < candidate && prime; j++) {
+		for(int j = 2; j < sqrt(candidate) && prime; j++) {
 			if(candidate % j == 0) {
 				prime = false;
 			}
@@ -25,28 +26,54 @@ int main() {
 	//		cout << candidate << ", ";
 		}
 	}
+//	cout << "ready for input" << endl;
 
-	//read the stupid inputs	
-	int sum_input, input, tmp;
-	string line;
-	int digit = 1;
-	getline(cin, line);
-	for(int i = line.length() - 1; i >= 0; i--) {
-		tmp = line[i] - 48;
-		sum_input = line[i];
-		input += digit * tmp;
-		digit = digit * 10;
+	int line;
+	while(cin >> line) {
+		line++;
+		while(!isSmith(line) || isPrime(line) || line == 1) {
+			line++;
+		}
+		cout << line << endl;
 	}
+}
+
+bool isPrime(int line) {
+	for(int j = 2; j < sqrt(line); j++)
+		if(line % j == 0)
+			return false;
+	return true;
+}
+
+bool isSmith(int line) {
+	//read the stupid inputs	
+	int sum_input = 0;
+	int input = line;
+	while(line > 0) {
+		sum_input += line % 10;
+		line /= 10;
+	}
+
 	
+//	cout << "splitting" << endl;
 	//split number in its prime factors
 	vector<int> prime_factors;
-	int sum_primes;
-	for(int i = 0; i < primes.size() && input > 1; i++) {
+	int sum_primes = 0;
+	for(unsigned int i = 0; i < primes.size() && input > 1; i++) {
 		while(input % primes[i] == 0 && input > 1) {
 			input /= primes[i];
 			prime_factors.push_back(primes[i]);
 			sum_primes += primes[i];
+			//cout << primes[i] << "-";
 		}
 	}
-	cout << "sum primes = " << sum_primes << ", sum input = " << sum_input << endl;
+	while(input > 0) {
+		sum_primes += input % 10;
+		input /= 10;
+	}
+
+	//cout << "sum primes = " << sum_primes << ", sum input = " << sum_input << endl;
+	return sum_primes == sum_input;
+	
 }
+
